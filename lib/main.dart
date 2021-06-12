d0f7d0c1537c056afa0d5399e1e805059f7b2243
Ananya Trivedi <ananyauw@gmail.com>
2021-06-12 14:45:32 -0600

    Dark Theme & Firebase

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar/res/event_firestore_service.dart';
+import 'package:flutter_calendar/ui/pages/add_event.dart';
+import 'package:flutter_calendar/ui/pages/view_event.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import 'model/event.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple[200],
+        // ignore: deprecated_member_use
+        accentColor: Colors.deepPurple[100],
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+      ),
+      home: HomePage(),
+      routes: {
+        "add_event": (_) => AddEventPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CalendarController _controller;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+    _events = {};
+    _selectedEvents = [];
+  }
+
+  Map<DateTime, List<dynamic>> _groupEvents(List<EventModel> allEvents) {
+    Map<DateTime, List<dynamic>> data = {};
+    allEvents.forEach((event) {
+      DateTime date = DateTime(
+          event.eventDate.year, event.eventDate.month, event.eventDate.day, 12);
+      if (data[date] == null) data[date] = [];
+      data[date].add(event);
+    });
+    return data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Calendar'),
+      ),
+      body: StreamBuilder<List<EventModel>>(
+          stream: eventDBS.streamList(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<EventModel> allEvents = snapshot.data;
+              if (allEvents.isNotEmpty) {
+                _events = _groupEvents(allEvents);
+              } else {
+                _events = {};
+                _selectedEvents = [];
+              }
+            }
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TableCalendar(
+                    events: _events,
+                    initialCalendarFormat: CalendarFormat.week,
+                    calendarStyle: CalendarStyle(
+                todayColor: Colors.deepPurple[200],
+                selectedColor: Colors.deepPurple[400],
+                // ignore: prefer_const_constructors
+                todayStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: Colors.black
+                )
+              ),
+                    headerStyle: HeaderStyle(
+                centerHeaderTitle: true,
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.deepPurple[400],
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                // ignore: prefer_const_constructors
+                formatButtonTextStyle: TextStyle(color: Colors.white),
+                formatButtonShowsNext: false,
+              ),
+                    startingDayOfWeek: StartingDayOfWeek.monday,
+                    onDaySelected: (date, events) {
+                      setState(() {
+                        _selectedEvents = events;
+                      });
+                    },
+                    builders: CalendarBuilders(
+                      selectedDayBuilder: (context, date, events) => Container(
+                          margin: const EdgeInsets.all(4.0),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Text(
+                            date.day.toString(),
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      todayDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple[400],
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      // ignore: prefer_const_constructors
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+              calendarController: _controller,
+            ),
+                  ..._selectedEvents.map((event) => ListTile(
+                        title: Text(event.title),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => EventDetailsPage(
+                                        event: event,
+                                      )));
+                        },
+                      )),
+                ],
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.pushNamed(context, 'add_event'),
+      ),
+    );
+  }
+}
